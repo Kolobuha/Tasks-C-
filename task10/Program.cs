bd0a6488106d2e70f45be0950c60cb9eaa64e4e5
Kolobuha <113272800+Kolobuha@users.noreply.github.com>
2022-09-26 15:42:18 +0500

    ДЗ семинар 2

---
+++ task10/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine ("Введите трехзначное число"); 
+int number = Convert.ToInt32(Console.ReadLine());  
+
+if (number > 999)
+    {
+    Console.WriteLine($"число {number} имеет более трех знаков");
+    }
+
+else if (number < 100)
+    {
+    Console.WriteLine($"число {number} имеет менее трех знаков");
+    }
+    
+else
+{
+Console.WriteLine(number); 
+int Two = (number / 10) % 10; 
+Console.WriteLine($"вторая цифра числа {number} это {Two}");
+}
---
+++ task13/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+
+Console.WriteLine ("введите число");  
+int Number = Convert.ToInt32(Console.ReadLine()); 
+int Number3 =-1;
+
+while (Number>99)
+{
+    Number3=Number%10;
+    Number=Number/10;
+}
+if (Number3>=0)
+    {
+        Console.WriteLine($"третья цифра {Number3}");
+    }
+else
+{
+    Console.WriteLine($"число {Number} не имеет третьего знака");
+}
