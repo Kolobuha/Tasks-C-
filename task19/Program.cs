eea42489fc4de5e2b6077e63817ff0918a79ce25
Kolobuha <113272800+Kolobuha@users.noreply.github.com>
2022-10-03 11:58:42 +0500

    ДЗ семинар 3

---
+++ task19/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 19 Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.WriteLine("Введите пятизначное число");
+int UserNumber = int.Parse(Console.ReadLine());
+int rNumber = 0;
+int modNumber = UserNumber;
+
+if (UserNumber > 10000 && UserNumber < 99999)
+{
+    while (modNumber > 0)
+    {
+        rNumber = (rNumber * 10) + (modNumber % 10);
+        modNumber = modNumber / 10;
+    }
+
+    if (rNumber == UserNumber)
+    {
+        Console.WriteLine($"число {UserNumber} является полиндромом");
+    }
+    else
+    {
+        Console.WriteLine($"число {UserNumber} не является полиндромом");
+    }
+}
+else
+{
+    Console.WriteLine($"число {UserNumber} не является пятизначным");
+}
---
+++ task23/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 23 Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+int UserNumber(string userInformation)
+{
+    int result = 0;
+    Console.Write($"{userInformation} ");
+    while (!int.TryParse(Console.ReadLine(), out result) || result <= 1)
+    {
+        Console.Write($"Ошибка ввода! Введите целое число больше единицы. {userInformation} ");
+    }
+    return result;
+}
+
+int number = UserNumber("Введите целое число больше единицы: ");
+
+int[] tableCubeNumber(int number)
+{
+    int[] result = new int [number];
+    for(int i = 1; i <= number; i++)
+    {
+        result[i - 1] = (int)(Math.Pow(i, 3));
+    }
+    return result;
+}
+
+int[] table = tableCubeNumber(number);
+
+foreach (int value in table)
+{
+    Console.WriteLine(value + " ");
+}
